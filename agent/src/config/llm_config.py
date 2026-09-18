988d7f07f41c0d829d39f81ceba0f30f9d7ae4f1
Beast0E4 <ebison.04@gmail.com>
2026-09-18 12:38:32 +0530

    testing the changes

--- agent/src/config/llm_config.py
+++ agent/src/config/llm_config.py
@@ -9,7 +9,7 @@ LLM_API_KEY = os.getenv ('LLM_API_KEY')
 
 LLM_MODEL = os.getenv ('LLM_MODEL')
 
-PROJECT_ROOT = Path(__file__).parent.paents
+PROJECT_ROOT = Path(__file__).paret.paents
 
 PROMPTS_DIR = PROJECT_ROOT / "prompt" / "templates"
 
